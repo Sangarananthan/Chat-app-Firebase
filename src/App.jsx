e58968f775a4a9425903356bc386252f7af215d9
Sangarananthan <sangarsmart16@gmail.com>
2024-07-03 05:25:44 +0530

    Initialized Project Files and Configured FireBase

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import 'rsuite/lib/styles/index.less'; // Import the Less file
+import './App.css';
+
+import './App.css';
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
