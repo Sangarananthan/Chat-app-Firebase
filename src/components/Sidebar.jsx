5bb2f19faec76578b490c1594d8631cbe2970921
Sangarananthan <sangarsmart16@gmail.com>
2024-07-22 11:43:13 +0530

    Started To work With Chat Room List

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,25 @@
+import { Divider } from 'rsuite';
 import CreateRoomBtnModal from './dashboard/CreateRoomBtnModal';
 import DashBoardToggle from './dashboard/DashBoardToggle';
+import ChatRoomList from './rooms/ChatRoomList';
+import { useEffect, useRef, useState } from 'react';
 
 const Sidebar = () => {
+  const topSideBarRef = useRef();
+  const [height, setheight] = useState(0);
+  useEffect(() => {
+    if (topSideBarRef.current) {
+      setheight(topSideBarRef.current.scrollHeight);
+    }
+  }, [topSideBarRef]);
   return (
     <div className="h-100 pt-2">
-      <div>
+      <div ref={topSideBarRef}>
         <DashBoardToggle />
         <CreateRoomBtnModal />
+        <Divider>Join Conversation</Divider>
       </div>
-      Bottom
+      <ChatRoomList aboveEleHeight={height} />
     </div>
   );
 };
---
+++ src/components/rooms/ChatRoomList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Nav } from 'rsuite';
+import RoomItems from './RoomItems';
+
+const ChatRoomList = ({ aboveEleHeight }) => {
+  return (
+    <Nav
+      appearance="subtle"
+      vertical
+      reversed
+      className="overflow-y-scroll custom-scroll"
+      style={{ height: `calc(100% - ${aboveEleHeight}px)` }}
+    >
+      <Nav.Item>
+        <RoomItems />
+      </Nav.Item>
+    </Nav>
+  );
+};
+
+export default ChatRoomList;
